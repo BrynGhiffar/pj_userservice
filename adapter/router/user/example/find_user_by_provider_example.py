d0939252e72deaf2f25efd1b4c25ff7644f1613b
Monique <monique.senjaya@binus.ac.id>
2022-10-29 23:16:09 +0700

    done

---
+++ adapter/router/user/example/find_user_by_provider_example.py
@@ -0,0 +1,28 @@
+# OPEN API EXAMPLE DOCUMENTATION
+FIND_USER_BY_PROVIDER_RESPONSE_EXAMPLE = {
+    "message": "users found",
+    "user": [
+                {
+                    "user_id": "634849b15be3416c39a868ad",
+                    "provider_id": 1,
+                    "provider": "Microsoft",
+                    "name": "Kang",
+                    "email": "kang@kong.com",
+                    "description": "Hi, my name is Kang. What's yours?? 🎈",
+                    "profile_pic": "123",
+                    "graduation_year": "2045",
+                    "current_semester": "5"
+                },
+                {
+                    "user_id": "123456b20be3416c39v854fe",
+                    "provider_id": 1,
+                    "provider": "Microsoft",
+                    "name": "Monique",
+                    "email": "Monique@gmail.com",
+                    "description": "Hi, I'm monique.",
+                    "profile_pic": "321412421",
+                    "graduation_year": "2024",
+                    "current_semester": "5"
+                },
+            ]
+}
