56c5e6b596d552b5da8ca38a7ed322f7507f8129
BrynGhiffar <bryn.ghiffar@gmail.com>
2022-12-07 08:50:32 +0700

    add find all user example

---
+++ adapter/router/user/example/find_all_user_example.py
@@ -0,0 +1,49 @@
+FIND_ALL_USER_EXAMPLE = {
+    "message": "found all users",
+    "users": [
+        {
+            "user_id": "638ce8431f42d0d9d02dc4b5",
+            "provider_id": "2c5c0d95-9abc-4df0-afe1-18fc68352346",
+            "provider": "MSFT",
+            "name": "BRYN ABYAN GHIFFAR",
+            "email": "bryn.ghiffar@binus.ac.id",
+            "description": "Hi, my name is Bryn Ghiffar. I am a Programmer for Binus University. I am a student, just like you, that's not cool.",
+            "profile_pic": "123",
+            "graduation_year": "2025",
+            "current_semester": "3"
+        },
+        {
+            "user_id": "638ce5b71f42d0d9d02dc4a3",
+            "provider_id": "2345",
+            "provider": "23456245",
+            "name": "Kang",
+            "email": "kang@as12dsdf.com",
+            "description": "Hi, my name is Kang",
+            "profile_pic": "123",
+            "graduation_year": "2045",
+            "current_semester": "5"
+        },
+        {
+            "user_id": "638cd8cec795a50c3ec4e05b",
+            "provider_id": "12322",
+            "provider": "MSFT",
+            "name": "asdf",
+            "email": "asd@gmail.com",
+            "description": "",
+            "profile_pic": "wqertasdgasdf",
+            "graduation_year": "2025",
+            "current_semester": "Y5"
+        },
+        {
+            "user_id": "638cce1cd04393024c2e0afa",
+            "provider_id": "123",
+            "provider": "23456245",
+            "name": "Kang",
+            "email": "kang@as12df.com",
+            "description": "Hi, my name is Kang YOWW",
+            "profile_pic": "123",
+            "graduation_year": "2045",
+            "current_semester": "5"
+        }
+    ]
+}
