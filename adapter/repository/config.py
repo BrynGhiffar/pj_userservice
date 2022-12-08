1315aecb0c41b86820dc0cfe06eb63f851d8a7c9
Monique Senjaya <monique.senjaya@binus.ac.id>
2022-12-08 15:05:37 +0700

    webhook url

--- adapter/repository/config.py
+++ adapter/repository/config.py
@@ -7,7 +7,7 @@ def get_database():
     CONNECTION_STRING = os.getenv("MONGO_URI")
     DB_NAME = os.getenv("MONGO_DB_NAME")
 
-    client = MongoClient(CONNECTION_STRING, tlsCAFile=ca)
+    client = MongoClient(CONNECTION_STRING)
     if not DB_NAME:
         DB_NAME = "mongodb://localhost:27017/"
 
