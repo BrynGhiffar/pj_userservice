f7829ef9a47b99259166c66db58641edfb39895b
Bryn Ghiffar <bryn.ghiffar@gmail.com>
2022-12-10 01:15:18 +0700

    refactor discord configuration

--- adapter/discord/config.py
+++ adapter/discord/config.py
@@ -1,11 +1,10 @@
-import aiohttp, os
-from discord import Webhook
+import os
 from dotenv import load_dotenv
 
 def get_webhook() -> str:
     load_dotenv()
-    WEBHOOK_URL = os.getenv("WEBHOOK_URL")
+    WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK")
     if not WEBHOOK_URL:
-        return "NO WEBHOOK"
+        return "DISCORD WEBHOOK NOT FOUND"
     else:
         return WEBHOOK_URL
